606bbc71bfc171d4b8fbae66227e7d423cc79a95
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2025-05-23 12:09:49 +0530

    Update testNumberToPair.h

--- testNumberToPair.h
+++ testNumberToPair.h
@@ -1,6 +1,10 @@
+#ifndef TEST_NUMBERTOPAIR
+#define TEST_NUMBERTOPAIR
+
 inline void ColorPairToString(const ColorPair* colorPair, char* buffer);
 inline ColorPair GetColorFromPairNumber(int pairNumber);
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor);
 
+#endif /*TEST_NUMBERTOPAIR*/
