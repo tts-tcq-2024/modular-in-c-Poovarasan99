e2042403daaa1a3f377750e19e6bed4581ef38cd
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2025-05-23 12:04:03 +0530

    Update testPairToNumber.c

--- testPairToNumber.c
+++ testPairToNumber.c
@@ -1,1 +1,17 @@
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
 
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
