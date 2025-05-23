fd4c6e90e863db96e89e59717e49e3a47dd7bc67
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2025-05-23 12:32:08 +0530

    Update and rename testNumberToPair.c to color_codes.c

---
+++ color_codes.c
@@ -0,0 +1,13 @@
+#include "color_codes.h"
+
+// Major and Minor color names
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
